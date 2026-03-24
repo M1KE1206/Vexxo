41a30cdf8ac80b831a3faf63409ea326ff21e371
Mike <davidmike185@gmail.com>
2026-03-24 03:32:33 +0100

    fix: add user to useEffect deps in ServiceRequestModal

--- src/components/ServiceRequestModal.jsx
+++ src/components/ServiceRequestModal.jsx
@@ -229,7 +229,7 @@ export default function ServiceRequestModal() {
     } else {
       setVisible(false);
     }
-  }, [isOpen, prefillData]);
+  }, [isOpen, prefillData, user]);
 
   // Escape key
   useEffect(() => {
